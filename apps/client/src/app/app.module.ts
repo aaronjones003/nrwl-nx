52e67ef3fe5b343473428237d170e459abb35d9f
Aaron Jones <ajones@catalystdevworks.com>
2018-08-31 10:49:50 -0400

    first commit

---
+++ apps/client/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/nx';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  initialState as appInitialState,
+  appReducer
+} from './+state/app.reducer';
+import { AppEffects } from './+state/app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    StoreModule.forRoot(
+      { app: appReducer },
+      {
+        initialState: { app: appInitialState },
+        metaReducers: !environment.production ? [storeFreeze] : []
+      }
+    ),
+    EffectsModule.forRoot([AppEffects]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
